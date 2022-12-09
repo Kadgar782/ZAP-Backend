82f7ec72782f7420f1c1f5a6a64fee6fd628d1eb
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-12-09 18:10:46 +0600

    Update data.controller.js

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -3,13 +3,32 @@ const Img = require('../models/img.model.js')
 const Product = require('../models/product.model.js')
 
 const getData = ((req, res) => {
-    Product.find({})
-        .then(result => res.status(200).json({ result }))
-        .catch(error => res.status(404).json({msg: error}))
-        //add another modules
+  const mapData = async () =>  {
+    const [posts, photos, comments] = await Promise.all([
+      Product.find({}),
+      Img.find({}),
+      Comment.find({}),
+    ]);
 
-})
+     posts.map((p) => { 
+      const avatars = photos.find((u) => u.id === p.userId); // userId in posts
+      //Add Comments
+      const commentsInPost = comments.find((u) => u.id === p.userId);
+       return {
+        ...p,
+        commentsInPost,
+        avatars,
+      }
+    });
+  };
+ const realData = async() =>{
 
+ }
+        (result) => res.status(200).json({ result })
+     .catch(error => res.status(404).json({msg: error}))
+ console.log(mapData())
+
+});
 
 module.exports = {
     getData
