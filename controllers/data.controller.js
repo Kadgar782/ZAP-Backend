3a89a0486266c49324ab45c6087e708f74c05984
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-12-14 20:17:28 +0600

    Update data.controller.js

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -2,37 +2,42 @@ const Comment = require('../models/comment.model.js')
 const Img = require('../models/img.model.js')
 const Product = require('../models/product.model.js')
 
-const getData = ((req, res) => {
-  const mapData = async () =>  {
+const getData = async (req, res) => {
+  const mapData = async () => {
     const [posts, photos, comments] = await Promise.all([
-      Product.find({}),
-      Img.find({}),
-      Comment.find({}),
+      Product.find({}).lean(),
+      Img.find({}).lean(),
+      Comment.find({}).lean(),
     ]);
- const mapPosts =()=> posts.map((p) => { 
-      const avatars = photos.find((u) => u.id === p.userId); // userId in posts
-      //Add Comments
-      const commentsInPost = comments.find((u) => u.id === p.userId);
-        return {
-        ...p,
-        commentsInPost,
-        avatars,
-      }
-    });
-    return  mapPosts()
-  };
+    console.log(photos);
+    console.log(posts);
+    console.log(comments);
 
-  
-  ( async function () {
-  console.log( await mapData());
+    const mapPosts = () =>
+      posts.map((p) => {
+        const avatars = photos.find((u) => u.id === p.userId); // userId in posts
+        //Add Comments
+        const commentsInPost = comments.find((u) => u.id === p.userId);
+        // console.log(posts);
+        // console.log(avatars);
+        // console.log(commentsInPost);
+        return {
+          ...p,
+          commentsInPost,
+          avatars,
+        };
+      });
+    return mapPosts();
+  };
+  mapData();
+
+  (async function () {
+    console.log(await mapData());
   })();
 
-
-        (result) => res.status(200).json({ result })
-     .catch(error => res.status(404).json({msg: error}))
- console.log(mapData())
-
-});
+  (result) =>res.status(200).json({ result })
+    .catch((error) => res.status(404).json({ msg: error }));
+};
 
 module.exports = {
     getData
