3329141f9714a04e39c6e9fae08f1dbdc74da597
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-12-15 19:10:53 +0600

    Update data.controller.js

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -1,44 +1,32 @@
-const Comment = require('../models/comment.model.js')
-const Img = require('../models/img.model.js')
-const Product = require('../models/product.model.js')
+const Comment = require("../models/comment.model.js");
+const Img = require("../models/img.model.js");
+const Product = require("../models/product.model.js");
 
 const getData = async (req, res) => {
-  const mapData = async () => {
+  try {
     const [posts, photos, comments] = await Promise.all([
       Product.find({}).lean(),
       Img.find({}).lean(),
       Comment.find({}).lean(),
     ]);
-    console.log(photos);
-    console.log(posts);
-    console.log(comments);
 
-    const mapPosts = () =>
-      posts.map((p) => {
-        const avatars = photos.find((u) => u.id === p.userId); // userId in posts
-        //Add Comments
-        const commentsInPost = comments.find((u) => u.id === p.userId);
-        // console.log(posts);
-        // console.log(avatars);
-        // console.log(commentsInPost);
-        return {
-          ...p,
-          commentsInPost,
-          avatars,
-        };
-      });
-    return mapPosts();
-  };
-  mapData();
+    const data = posts.map((p) => {
+      const avatars = photos.find((u) => u.id === p.id);
+      //Add Comments
+      const commentsInPost = comments.find((u) => u.id === p.id);
+      return {
+        ...p,
+        commentsInPost,
+        avatars,
+      };
+    });
 
-  (async function () {
-    console.log(await mapData());
-  })();
-
-  (result) =>res.status(200).json({ result })
-    .catch((error) => res.status(404).json({ msg: error }));
+    res.status(200).json({ data });
+  } catch (error) {
+    res.status(404).json({ msg: error });
+  }
 };
 
 module.exports = {
-    getData
-}
+  getData,
+};
