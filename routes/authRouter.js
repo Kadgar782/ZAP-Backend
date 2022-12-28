f3fe42b75af54e1591ba9b77ce4f898c673933ba
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-12-28 17:38:57 +0600

    Add files via upload

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,12 +2,14 @@ const Router = require('express')
 const router = new Router()
 const controller = require('../controllers/auth.controller')
 const {check} = require("express-validator")
+const authMiddlewaree = require("../middlewaree/authMiddlewaree")
 
 router.post('/registration', [
     check('username', "Username can't be empty").notEmpty(),
     check('password', "Password must be more than 4 characters and less than 10").isLength({min:4, max:10})
 ], controller.registration)
 router.post('/login', controller.login)
-router.get('/users', controller.getUsers)
+router.get('/users', authMiddlewaree, controller.getUsers)
+
 
 module.exports = router
