5019e346beb42200e920b84dfa3e28e6923b2bef
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2023-02-17 18:42:28 +0600

    Add files via upload

--- middlewaree/authMiddlewaree.js
+++ middlewaree/authMiddlewaree.js
@@ -1,21 +1,18 @@
 const jwt = require("jsonwebtoken")
 const {secret} = require("../config")
 
-module.exports = function (req, res, next) {    
-    console.log(req.headers.authorization) 
-   
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        console.log(token) 
-         if (!token) {
-            next() 
-         }
-         const decodeData = jwt.verify(token, secret)
-         req.user = decodeData
-         next() 
-    } catch(e) {
-        console.log(e)
-        return res.status(403).json({message:"User is not loged in"})
-    }
+module.exports = function (req, res, next) {
+  console.log(req.headers.authorization);
 
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    console.log(token);
+    if (token) {
+      const decodeData = jwt.verify(token, secret);
+      req.user = decodeData;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  next();
 }; 
