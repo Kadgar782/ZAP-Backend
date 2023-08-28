35a6d20b04178a2e58390d68f7c660b197484a09
Kadgar782 <bogdan7823@mail.ru>
2023-08-28 19:19:50 +0600

    Update for netify

--- netify/functions/index.js
+++ netify/functions/index.js
@@ -1,10 +1,11 @@
+import serverless from 'serverless-http';
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const authRouter = require('../routes/authRouter')
-const products_routes = require('../routes/products.js')
-const comments_routes = require('../routes/comment.routes.js')
-const data_routes = require('../routes/data.routes.js')
+const authRouter = require('../../routes/authRouter')
+const products_routes = require('../../routes/products.js')
+const comments_routes = require('../../routes/comment.routes.js')
+const data_routes = require('../../routes/data.routes.js')
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
 
@@ -27,4 +28,5 @@ app.use('/api/products', products_routes)
 app.use('/api/comments', comments_routes)
 app.use('/api/data',data_routes)
 
+export const handler = serverless(app);
 
